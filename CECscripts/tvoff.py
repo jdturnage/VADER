3f2cbce1a1d6e2b1c258d7731005aa9d9ed613b5
James D Turnage <james.d.turnage@nasa.gov>
2013-08-12 10:11:47 -0500

    Hayley's final changes + cleanup
    This commit is simply migrating all of the updates from Hayley's github
    to mine. This will also get rid of unnecessary files for Digital Signage
    branch.

--- CECscripts/tvoff.py
+++
@@ -1,8 +0,0 @@
-import sys, xbmc, xbmcgui, subprocess
-
-class MyClass(xbmcgui.Window):
-	def __init__(self):
-		subprocess.call("echo 'standby 0' | cec-client -s", shell=True)
-
-myTestScript = MyClass()
-del myTestScript
--- MoCoCen/Scripts/serial-switchVADER.py
+++
@@ -1,5 +0,0 @@
-import serial, binascii, sys, time
-
-#HDMI 1
-VADERSWITCH = "\x20\x02\x01\x01\xa5"
-serial.Serial(6,9600,timeout = 1).write(VADERSWITCH)
